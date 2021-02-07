53b219d9c64bca47cb3058466779236cd3a3ba93
ilimkanu <ilimkanu@gmail.com>
2021-02-07 15:34:30 +0600

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+import static java.lang.Math.*;
+
+public class Calculator {
+    public void main(){
+        System.out.println(Math.abs(-23));
+        System.out.println(Math.pow(4,2));
+        System.out.println(Math.sqrt(36));
+        System.out.println(Math.round(9.5000001));
+        System.out.println(Math.cbrt(125));
+    }
+}
